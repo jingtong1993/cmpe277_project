d9ee9514ed558cb3f8b7cb11df1f23776b8aa545
lingzhuo <ling>
2020-04-06 17:35:00 -0700

    用户界面布局

--- App/chatEats/app/src/main/java/com/example/chatEats/push/activities/UserActivity.java
+++ App/chatEats/app/src/main/java/com/example/chatEats/push/activities/UserActivity.java
@@ -1,19 +1,39 @@
 package com.example.chatEats.push.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.ViewTarget;
 import com.example.chatEats.common.app.Activity;
 import com.example.chatEats.common.app.Fragment;
 import com.example.chatEats.push.R;
 import com.example.chatEats.push.frags.user.UpdateInfoFragment;
 
+import net.qiujuer.genius.ui.compat.UiCompat;
+
+import androidx.core.graphics.drawable.DrawableCompat;
+import butterknife.BindView;
+
 public class UserActivity extends Activity {
     private Fragment mCurFragment;
+    @BindView(R.id.im_bg)
+    ImageView mBg;
+
+    public static void show(Context context) {
+        context.startActivity(new Intent(context, UserActivity.class));
+    }
 
     @Override
     protected int getContentLayoutId() {
         return R.layout.activity_user;
     }
+    //activity_user里面是约束布局，现在要提供user修改， 所以修改布局
 
     @Override
     protected void initWidget() {
@@ -24,6 +44,21 @@ public class UserActivity extends Activity {
                 .beginTransaction()
                 .add(R.id.lay_container, mCurFragment)
                 .commit();
+
+        Glide.with(this)
+                .load(R.drawable.bg_src_tianjin)
+                .centerCrop()
+                .into(new ViewTarget<ImageView, GlideDrawable>(mBg) {
+                    @Override
+                    public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
+                        Drawable drawable = resource.getCurrent();
+                        drawable = DrawableCompat.wrap(drawable);
+                        drawable.setColorFilter(UiCompat.getColor(getResources(), R.color.colorAccent),
+                                PorterDuff.Mode.SCREEN);
+
+                        this.view.setImageDrawable(drawable);
+                    }
+                });
     }
 
     // Activity中收到剪切图片成功的回调
