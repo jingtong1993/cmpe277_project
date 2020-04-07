e98917ee3f5b5d9f516a119b3ac44ea694f345f5
lingzhuo <ling>
2020-04-07 16:57:24 -0700

    Update Common.java

--- App/chatEats/common/src/main/java/com/example/chatEats/common/Common.java
+++ App/chatEats/common/src/main/java/com/example/chatEats/common/Common.java
@@ -6,9 +6,6 @@ public class Common {
         //String REGEX_MOBILE = "[1][3,4,5,7,8][0-9]{9}$";
 
         //String API_URL = "http://192.168.29.254:8080/api/";
-        String API_URL = "http://127.0.0.1:8080/api/";
-
-
+        String API_URL = "http://192.168.1.2:8080/api/";
     }
-
 }
